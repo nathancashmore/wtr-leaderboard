94fec2e31113b8265ba145bf244b05b75c5a7080
nathancashmore <nathan.cashmore@staticvoid.co.uk>
2018-09-23 21:17:31 +0100

    Add button endpoint

--- app/app.js
+++ app/app.js
@@ -10,6 +10,7 @@ const DataHelper = require('./helper/data-helper');
 
 const indexRouter = require('./routes/index');
 const timeRouter = require('./routes/time');
+const buttonRouter = require('./routes/button');
 
 const app = module.exports = express();
 
@@ -33,6 +34,7 @@ app.use(bodyParser.json());
 
 app.use('/', indexRouter);
 app.use('/time', timeRouter);
+app.use('/buttons', buttonRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
---
+++ test/button.spec.js
@@ -0,0 +1,117 @@
+const bootstrap = require('./bootstrap');
+const chai = require('chai');
+const chaiHttp = require('chai-http');
+const moment = require('moment');
+
+chai.use(chaiHttp);
+
+let server = require('../app/app');
+
+chai.use(chaiHttp);
+
+// NOTE : This test requires all of the tests to be run in sequence to test
+// the scoring as each button is hit.
+
+let today = moment().format('YYYY-MM-DD');
+let yesterday = moment().subtract('days', 1).format('YYYY-MM-DD');
+
+describe('Button', function () {
+	describe('Day 1', function () {
+
+
+		before(async () => {
+			// Make sure the start date is set to today so
+			// we know that the day will be 0
+
+			await bootstrap.withoutButtonHistoryData();
+			await bootstrap.withStartDate(today);
+		});
+
+
+		it('it should POST to record press for button 1 on day 0', (done) => {
+			const endpoint = '/buttons/1';
+
+			chai.request(server)
+				.post(endpoint)
+				.end((err, res) => {
+					expect(res.status).to.equal(200);
+					expect(res.body.button).to.equal(1);
+					expect(res.body.day).to.equal(0);
+					expect(res.body.team).to.equal(1);
+					expect(res.body.score).to.equal(3);
+					done();
+				});
+		});
+
+		it('it should POST to record press for button 2 on day 0', (done) => {
+			const endpoint = '/buttons/2';
+
+			chai.request(server)
+				.post(endpoint)
+				.end((err, res) => {
+					expect(res.status).to.equal(200);
+					expect(res.body.button).to.equal(2);
+					expect(res.body.day).to.equal(0);
+					expect(res.body.team).to.equal(2);
+					expect(res.body.score).to.equal(2);
+					done();
+				});
+		});
+
+		it('it should POST to record press for button 3 on day 0', (done) => {
+			const endpoint = '/buttons/3';
+
+			chai.request(server)
+				.post(endpoint)
+				.end((err, res) => {
+					expect(res.status).to.equal(200);
+					expect(res.body.button).to.equal(3);
+					expect(res.body.day).to.equal(0);
+					expect(res.body.team).to.equal(3);
+					expect(res.body.score).to.equal(1);
+					done();
+				});
+		});
+	});
+	describe('Day 2', function () {
+
+		before(async () => {
+			// Make sure the start date is set to today so
+			// we know that the day will be 0
+			await bootstrap.withStartDate(yesterday);
+		});
+
+
+		it('it should POST to record press for button 3 on day 1', (done) => {
+			const endpoint = '/buttons/3';
+
+			chai.request(server)
+				.post(endpoint)
+				.end((err, res) => {
+					expect(res.status).to.equal(200);
+					expect(res.body.button).to.equal(3);
+					expect(res.body.day).to.equal(1);
+					expect(res.body.team).to.equal(1);
+					expect(res.body.score).to.equal(3);
+					done();
+				});
+		});
+
+		it('it should POST to record press for button 2 on day 1', (done) => {
+			const endpoint = '/buttons/2';
+
+			chai.request(server)
+				.post(endpoint)
+				.end((err, res) => {
+					expect(res.status).to.equal(200);
+					expect(res.body.button).to.equal(2);
+					expect(res.body.day).to.equal(1);
+					expect(res.body.team).to.equal(3);
+					expect(res.body.score).to.equal(2);
+					done();
+				});
+		});
+
+	});
+});
+
