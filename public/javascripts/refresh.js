70e4587ad2280256faa1aa46098d23eaad194659
Nate <nathan.cashmore@staticvoid.co.uk>
2018-10-13 13:01:44 +0100

    Fix progress bar refresh (#24)

--- public/javascripts/refresh.js
+++ public/javascripts/refresh.js
@@ -2,7 +2,7 @@
 function reload() {
   console.log("RELOAD");
   $('#score-table').load(`${location.href}score-table`, ''); // eslint-disable-line
-  $('#progress').load(`${location.href}update`, ''); // eslint-disable-line
+  $('#progress-section').load(`${location.href}/update`, ''); // eslint-disable-line
 }
 
 setInterval(reload, 3000);
