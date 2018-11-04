6ab598fd2f673122d47dce8230c0886bc5ae0087
nathancashmore <nathan.cashmore@staticvoid.co.uk>
2018-11-04 17:27:17 +0000

    Add status code for deployment on rasp pi

--- button-ideas/raspberry-pi/status.py
+++ button-ideas/raspberry-pi/status.py
@@ -8,17 +8,22 @@
 import time
 import requests
 import socket
+import json
 
-ButtonNumber = socket.gethostname().split("-")[1]
-
-print("Button number determined as : " + str(ButtonNumber))
-
-url = 'https://iot-hunt.herokuapp.com/status/' + str(ButtonNumber)
+url = 'https://iot-hunt.herokuapp.com/status'
 headers = {'content-type': 'application/json', 'Accept-Charset': 'UTF-8'}
+
+def getIp():
+    s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    s.connect(("8.8.8.8", 80))
+    ip_address = s.getsockname()[0]
+    s.close()
+    return ip_address
 
 def loop():
     while True:
-        r = requests.post(url, headers=headers)
+        data = {'button': ButtonNumber, 'ip': getIp()}
+        r = requests.put(url, headers=headers, data=json.dumps(data))
         print(r.json)
         time.sleep(540)   # Wait for 9 mins
 
@@ -26,6 +31,11 @@ def destroy():
     print("Status closing")
 
 if __name__ == '__main__':     # Program start from here
+    ButtonNumber = socket.gethostname().split("-")[1]
+
+    print("Button number determined as : " + str(ButtonNumber))
+    print("IP determined as : " + str(getIp()))
+
     try:
         loop()
     except KeyboardInterrupt:  # When 'Ctrl+C' is pressed, the child program destroy() will be  executed.
