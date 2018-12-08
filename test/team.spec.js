c178f18546a25d887401eea0f89c44d49aec6ba4
tpuncash <nathan.cashmore@waitrose.co.uk>
2018-12-08 21:11:56 +0000

    Remove team pages to prevent people pressing buttons before they receive their clue

--- test/team.spec.js
+++ test/team.spec.js
@@ -69,11 +69,15 @@ describe('Team Integration', () => {
         await page.close();
       });
 
-      it('should have the correct page title', async () => {
+      it('should return 404 for teams during the event', async () => {
+        expect(await testHelper.getText(page, 'error')).to.contain('404');
+      });
+
+      xit('should have the correct page title', async () => {
         expect(await page.title()).to.eql(i18n.__('title'));
       });
 
-      it('should have the correct team name', async () => {
+      xit('should have the correct team name', async () => {
         const TAG = '[data-test="team-name"]';
         await page.waitFor(TAG);
 
@@ -82,7 +86,7 @@ describe('Team Integration', () => {
         expect(text.replace('\t', '')).to.eql(i18n.__('team-1'));
       });
 
-      it('should have the correct team score', async () => {
+      xit('should have the correct team score', async () => {
         const TAG = '[data-test="team-score"]';
         await page.waitFor(TAG);
 
@@ -91,7 +95,7 @@ describe('Team Integration', () => {
         expect(text.replace('\t', '')).to.eql(`${expectedScores[1].score}`);
       });
 
-      it('should have the correct button name', async () => {
+      xit('should have the correct button name', async () => {
         const TAG = '[data-test="button-name"]';
         await page.waitFor(TAG);
 
@@ -100,7 +104,7 @@ describe('Team Integration', () => {
         expect(text.replace('\t', '')).to.eql(i18n.__('button-name-3'));
       });
 
-      it('should have the correct clue', async () => {
+      xit('should have the correct clue', async () => {
         const TAG = '[data-test="button-clue"]';
         await page.waitFor(TAG);
 
@@ -109,12 +113,12 @@ describe('Team Integration', () => {
         expect(text.replace('\t', '')).to.eql(i18n.__('button-clue-3'));
       });
 
-      it('should have a link back to the leaderboard', async () => {
+      xit('should have a link back to the leaderboard', async () => {
         await page.click('[data-test="leaderboard-button"]');
         expect(page.url()).to.contain('/leaderboard');
       });
 
-      it('should only return valid teams', async () => {
+      xit('should only return valid teams', async () => {
         await page.goto('http://localhost:3000/teams/0');
         expect(await testHelper.getText(page, 'error')).to.contain('404');
 
